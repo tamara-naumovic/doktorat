a2f72aec8216595cea14868702368e310fbacdd8
Tamara Naumović <tamaranaumovic@gmail.com>
2022-04-21 18:01:01 +0200

    kreiran novi test format

--- test_simulator.py
+++ test_simulator.py
@@ -1,9 +1,9 @@
 from opcije_simulacije import OpcijeSimulacije
-from simulator import Simulator
+# from simulator import Simulator
 import sim
 
 def main():
-    opcije = OpcijeSimulacije("tk1.csv", 0.1, 0.1, 10)
+    opcije = OpcijeSimulacije("test_data/tk2.csv", 0.1, 0.1, 10)
 
     # Simulator.ucitaj_blokove(opcije)
     # print(opcije.niz_blokova)
@@ -12,13 +12,18 @@ def main():
     # Simulator.obradi_niz_blokova(opcije)
     # print(opcije.niz_obradjen)
     sim.ucitaj_blokove(opcije)
-    print(opcije.niz_blokova)
+    print(*opcije.niz_blokova)
+    print("----------------Niz blokova-------------------")
     sim.kreiraj_blokove(opcije)
-    print(opcije.niz_blokova)
+    print(*opcije.niz_blokova,sep="\n")
+    print("----------------Obradjen niz blokova-------------------")
+
     sim.obradi_niz_blokova(opcije)
-    print(opcije.niz_obradjen)
+    print(*opcije.niz_obradjen,sep="\n")
+    print("----------------Sortiran niz blokova-------------------")
+
     sim.sortiraj_niz(opcije)
-    print(opcije.niz_sortiran)
+    print(*opcije.niz_sortiran,sep="\n")
 
 
 if __name__ == "__main__":
