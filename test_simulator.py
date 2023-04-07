94f1f8238fe0a029e056c40c517e470ce02c4510
Tamara Naumovic <tamaranaumovic@gmail.com>
2023-04-07 20:48:51 +0200

    izmena za decimal.Decimal

--- test_simulator.py
+++ test_simulator.py
@@ -3,23 +3,23 @@ import sim, json, pprint, time
 
 def main():
     pocetak = time.time()
-    # opcije = OpcijeSimulacije(tabela_konfiguracije="test_data/tk11.csv", interval_stampanja= 0.1, interval_integracije= 0.1, duzina_simulacije= 10)
+    # opcije = OpcijeSimulacije(tabela_konfiguracije="testPodaci/tk11.csv", interval_stampanja= 0.1, interval_integracije= 0.1, duzina_simulacije= 10)
     # student test
-    opcije = OpcijeSimulacije(tabela_konfiguracije="test_data/tk2.csv", interval_stampanja= 0.1, interval_integracije= 0.1, duzina_simulacije= 10)
+    opcije = OpcijeSimulacije(tabela_konfiguracije="testPodaci/tk2.csv", interval_stampanja= 0.1, interval_integracije= 0.1, duzina_simulacije= 10)
     print("----------------Niz blokova-------------------")
     sim.ucitaj_blokove(opcije)
-    print(json.dumps(opcije.niz_blokova, default=print))
+    print(json.dumps(opcije.niz_blokova, default=print, cls=sim.DecimalEncoder))
     # pprint.pprint(opcije.niz_blokova)
     print("----------------Obradjen niz blokova-------------------")
     sim.obradi_niz_blokova(opcije)
-    print(json.dumps(opcije.niz_obradjen, default=print))
+    print(json.dumps(opcije.niz_obradjen, default=print, cls=sim.DecimalEncoder))
     
     print("----------------Niz Rb integratora-------------------")
     print(opcije.niz_rb_integratora)
     
     print("----------------Sortiran niz blokova-------------------")
     sim.sortiraj_niz(opcije)
-    print(json.dumps(opcije.niz_sortiran, default=print))
+    print(json.dumps(opcije.niz_sortiran, default=print,cls=sim.DecimalEncoder))
     # print("----------------Niz izlaza-------------------")
 
     # sim.generisi_izlaz_indekse(opcije)
@@ -38,12 +38,12 @@ def main():
     sim.racunaj(opcije)
     # print(json.dumps(opcije.matrica_izlaza, indent=4))
     print("----------------Matrica izlaza-------------------")
-    print(json.dumps(opcije.matrica_izlaza, indent=4))
+    print(json.dumps(opcije.matrica_izlaza, indent=4,cls=sim.DecimalEncoder))
     file_name = opcije.tabela_konfiguracije.split("/")[-1][:-4]
-    result_file_name = f"result_data/result-{file_name}.json"
+    result_file_name = f"rezultati/result-{file_name}.json"
     print(f"naziv fajla: {result_file_name}")
     with open(result_file_name, 'w') as fp:
-        json.dump(opcije.matrica_izlaza, fp)
+        json.dump(opcije.matrica_izlaza, fp,cls=sim.DecimalEncoder)
     kraj = time.time()
     print(f"Duzina simulacije: {kraj-pocetak}")
     # print("----------------Izlazi-------------------")
